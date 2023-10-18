4f8eec6d62430a583dd6a1bc65d82200fe5c3044
Ricardo Coelho <ricms_coelho@hotmail.com>
2023-10-18 11:42:52 +0100

    First commit: gitignore + script

--- comment-replace.js
+++ comment-replace.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  substituir comentários de certos users por factos aleatórios
-// @author       You
+// @author       rcoelho14
 // @match        https://www.cmportugal.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cmportugal.com
 // @grant        none
