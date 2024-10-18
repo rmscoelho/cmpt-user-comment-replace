604e64b2d9713478f875f55d809e7da3642d4a8d
rmscoelho <ricms_coelho@hotmail.com>
2024-10-18 02:34:41 +0100

    new code

--- comment-replace.js
+++ comment-replace.js
@@ -6,60 +6,51 @@
 // @author       rcoelho14
 // @match        https://www.cmportugal.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cmportugal.com
-// @grant        none
+// @grant        GM_addElement
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-(function () {
+(async function () {
     'use strict';
 
-    const userList = ['Sima01']
+    const userList = ['Sima01', 'Santon', 'AVB2024', 'Bug', 'Tibraco']
     const getComment = document.querySelectorAll('article.ipsComment > aside.ipsComment_author > h3 > strong > a')
     const getQuote = document.querySelectorAll('blockquote');
 
-    //Escolha entre: facts/aleixo/renato/senhorPe/beatriz/inspirational/jokes/biblia/memes
-    const quoteType = 'facts'
+    //Escolha entre: facts/aleixo/renato/senhorPe/beatriz/inspirational/jokes/biblia/memes/capybaraFacts/capybaraImages
+    const quoteType = 'capybaraImages'
+
+    console.log(await randomFacts(quoteType))
 
     // Replace comments from user with random facts
-    getComment.forEach((element) => {
+    for (const element of getComment) {
         let commentParent = element.parentElement.parentElement.parentElement.parentElement;
-        userList.forEach(user => {
+        for (const user of userList) {
             if (element.text === user) {
-                commentParent.getElementsByClassName('ipsType_richText')[0].innerHTML = randomFacts(quoteType)
+                let fact = await randomFacts(quoteType);
+                commentParent.getElementsByClassName('ipsType_richText')[0].innerHTML = (quoteType === 'memes') ? "<img src='" + fact + "'/>" : "<p>\t&#127775; " + fact + " \t&#127775;</p>"
             }
-        })
-    })
+        }
+    }
 
     // Replace quoted comments from user with random facts
-    getQuote.forEach(element => {
-        userList.forEach(user => {
+    for (const element of getQuote) {
+        for (const user of userList) {
             if (element.getElementsByClassName('ipsQuote_citation')[0].innerHTML.indexOf(user) !== -1) {
                 let citationContent = element.children[1];
-                citationContent.innerHTML = randomFacts(quoteType)
+                let fact = await randomFacts(quoteType);
+                citationContent.innerHTML = (quoteType === 'memes') ? "<img src='" + fact + "'/>" : "<p>\t&#127775; " + fact + " \t&#127775;</p>"
             }
-        })
-    })
+        }
+    }
 })();
 
-async function randomFacts(quoteType = 'facts') {
+async function randomFacts(quoteType = 'memes') {
     const url = "https://raw.githubusercontent.com/rmscoelho/cmpt-user-comment-replace/refs/heads/master/replacementText.json";
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
 
-        const json = await response.json();
-        console.log(json[quoteType]);
+    const getRequest = await GM.xmlHttpRequest({url: url}).catch(e => console.error(e));
+    let response = JSON.parse(getRequest.responseText);
 
-        if (json[quoteType] === 'memes') {
-            return
-            `<img src=${json[quoteType][Math.floor((Math.random() * data.length))]}>`
-            ;
-        } else {
-            return "<p>\t&#127775; " + json[quoteType][Math.floor((Math.random() * data.length))] + " \t&#127775;</p>"
-        }
+    return response[quoteType][Math.floor((Math.random() * response[quoteType].length))]
 
-    } catch (error) {
-        console.error(error.message);
-    }
 }
